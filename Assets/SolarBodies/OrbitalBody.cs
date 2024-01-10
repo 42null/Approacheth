592e85d1ba9ef41a033afd3b7abf10e1e0fbb128
42null <averyparanoiduser@gmail.com>
2024-01-10 09:55:52 -0600

    Bugfix inspector added orbiters not moving (fixed)

--- Assets/SolarBodies/OrbitalBody.cs
+++ Assets/SolarBodies/OrbitalBody.cs
@@ -5,11 +5,30 @@ using SolarBodies;
 using Unity.VisualScripting;
 using UnityEditor.TextCore.Text;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
+
+[System.Serializable]
+public class OrbiterData
+{
+    public GameObject Orbiter;
+    public float Distance;
+    
+    // public GameObject Orbiter => orbiter;
+    // public float Distance => distance;
+
+    public OrbiterData(GameObject orbiter, float distance)
+    {
+        this.Orbiter = orbiter;
+        this.Distance = distance;
+    }
+
+}
 
 public class OrbitalBody : MonoBehaviour
 {
-    private List<KeyValuePair<GameObject, float>> orbiters = new List<KeyValuePair<GameObject, float>>();
+    // [SerializeField] public List<KeyValuePair<GameObject, float>> orbiters = new List<KeyValuePair<GameObject, float>>();
+    [SerializeField] public List<OrbiterData> orbiters = new List<OrbiterData>();
 
     [SerializeField] [Range(0f, 10f)] public float scale = 1f;
     
@@ -17,7 +36,7 @@ public class OrbitalBody : MonoBehaviour
     private ResourceHolder resourceHolder;
     private float orbitingAtAngle = 0;
     
-    private Transform transform = new RectTransform();
+    private Transform transform;
 
     public float getAngle()
     {
@@ -27,7 +46,7 @@ public class OrbitalBody : MonoBehaviour
     {
         return this.orbitingAtAngle += angle;
     }
-    
+
     public OrbitalBody()
     {
         this.resourceHolder = NaturalResourceGen.generateResources(NaturalResourceGen.BodyType.STAR, scale);
@@ -38,19 +57,20 @@ public class OrbitalBody : MonoBehaviour
     {
         this.transform = GetComponent<Transform>();
         float orbiterDistance = (newOrbiter.transform.position - this.transform.position).magnitude;
-
-        orbiters.Add(new KeyValuePair<GameObject, float>(newOrbiter, orbiterDistance));
+        
+        orbiters.Add(new OrbiterData(newOrbiter, orbiterDistance));
     }
     
     private void OnValidate()
     {
+    
         // this.transform.localScale = new Vector3(scale/10, scale/10, 1);
     }
     void Start()
      {
 
      }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -58,9 +78,9 @@ public class OrbitalBody : MonoBehaviour
         foreach (var orbiter in orbiters)
         {
             //TODO: Fix constent grabbing of GetComponent
-            OrbitalBody orbitalBodyScript = orbiter.Key.GetComponent<OrbitalBody>();
+            OrbitalBody orbitalBodyScript = orbiter.Orbiter.GetComponent<OrbitalBody>();
             orbitalBodyScript.addToAngle(Time.deltaTime);
-            orbiter.Key.transform.position = OrbitalMechanics.CaculateNewPos(orbiter.Key,this.transform.gameObject, orbitalBodyScript.getAngle(), orbiter.Value);
+            orbiter.Orbiter.transform.position = OrbitalMechanics.CaculateNewPos(orbiter.Orbiter,this.gameObject, orbitalBodyScript.getAngle(), orbiter.Distance);
             
         }
 
